beae0196e999eb56ed939fee90bffdb6fc2f30f8
Ritesh Niwane <riteshniwane007@gmail.com>
2026-08-02 15:39:57 +0530

    feat: implement Agent class with message history and role-based communication support

--- ai_sdk/agent.py
+++ ai_sdk/agent.py
@@ -1,3 +1,27 @@
-
+from ai_sdk.message import Message
+from dataclasses import dataclass
+from ai_sdk.enums import Role
+@dataclass
 class Agent:
-    pass
+    name:str
+    system_prompt:str
+    messages:list[Message]
+
+    def __init__(self,name:str,system_prompt:str):
+        self.name=name
+        self.system_prompt=system_prompt
+        self.messages=[Message(Role.SYSTEM,system_prompt)]
+    
+    def chat(self,message:str)->Message:
+        self.messages.append(Message(Role.USER,message))
+        llm_message=self.call_llm(message)
+        self.messages.append(llm_message)
+        return llm_message
+
+    def call_llm(self,message:str)->Message:
+        return Message(Role.ASSISTANT,f"Echo: {message}")
+
+    def history(self)->list[Message]:
+        return self.messages
+
+
